b997e2d2a96b60272b35b26ca413d0bc617681b4
hyungdoyou <yhd4286@gmail.com>
2023-11-27 19:55:50 +0900

    [Docs] 소스 코드 수정
    코드 수정

--- src/main/java/ClientFileTest.java
+++ src/main/java/ClientFileTest.java
@@ -6,7 +6,7 @@ public class ClientFileTest {
 
     public static void main(String[] args) {
         try {
-            Socket yhd = new Socket("192.168.0.114", 7777);
+            Socket yhd = new Socket("192.168.88.1", 9999);
 
             // 여기부터 요청하는 코드
             System.out.print("서버에 요청할 메시지를 입력하세요 : ");
@@ -20,24 +20,17 @@ public class ClientFileTest {
             // 여기까지 요청하는 코드
 
             // 여기부터 요청받는 코드
-            //InputStream cis = yhd.getInputStream();
-            //InputStreamReader isr = new InputStreamReader(cis);
-            //BufferedReader br = new BufferedReader(isr);
             InputStream cis = yhd.getInputStream();
             BufferedInputStream brs = new BufferedInputStream(cis);
-;
 
-
-            //FileInputStream fileInputStream =
             FileOutputStream fileOutputStream = new FileOutputStream("c:\\test2\\" + test2);
             byte[] bytes = brs.readAllBytes();
-            //fileOutputStream.write(bytes);
             for(int i=0; i<bytes.length; i++) {
                 fileOutputStream.write(bytes[i]);
                 System.out.print(bytes[i]);
             }
-
             // 여기까지 요청받는 코드
+
             System.out.print("요청이 이뤄졌습니다.");
 
             yhd.close();
